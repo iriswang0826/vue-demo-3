6cb571fc36f297616ecee0c5aa463dcf28437c2e
Iris Wang <iriswang1224@163.com>
2017-08-29 15:41:14 +0800

    计算属性，属性监听

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,65 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+  <div>
+    <input type="text" v-model="myValue">
+    <!-- {{ myValueWithoutNum }} -->
+    {{ getMyValWihoutNum() }}
+    <ul>
+      <li v-for="item in myList">{{item.name}} + {{item.price}}</li>
+    </ul>
+    <button @click="changeList">change</button>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
 export default {
-  name: 'app'
+  data () {
+    return {
+      myValue: '',
+      myList: [
+        {
+          name: 'apple',
+          price: 34
+        },
+        {
+          name: 'banana',
+          price: 36
+        }
+      ]
+    }
+  },
+  computed: {
+    myValueWithoutNum: function () {
+      // return this.myValue.replace(/\d/g, '')
+      return Date.now()
+    }
+  },
+  methods: {
+    getMyValWihoutNum () {
+      return this.myValue.replace(/\d/g, '')
+    },
+    changeList () {
+      Vue.set(this.myList, 1, {
+        name: 'pinapple',
+        price: 78
+      })
+      // this.tellUser()
+    },
+    removeItem () {
+      // this.tellUser()
+    },
+    tellUser () {
+      alert('list change')
+    }
+  },
+  watch: {
+    myValue: function (val, oldVal) {
+      console.log(val, oldVal)
+    },
+    myList: function () {
+      this.tellUser()
+    }
+  }
 }
 </script>
 
